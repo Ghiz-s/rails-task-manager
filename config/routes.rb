b1a5d5e4dc33790c660a345738281bc433ba5bfe
Ghizlane Siebenmann <ghizlane.siebenmann@hotmail.ch>
2020-11-18 11:05:31 +0100

    commit after kitt bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+# comment to fix kitt bug
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'tasks', to: 'tasks#index'
